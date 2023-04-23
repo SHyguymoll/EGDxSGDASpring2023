324991505fa78edff516856af82294a615e7b015
SHyguymoll <shymollet@gmail.com>
2023-04-23 04:19:42 -0700

    Web preset, the grass is greener, camera scrolling is too finicky so I disabled it

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		if event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
-			position -= event.relative * zoom
+#func _unhandled_input(event):
+#	if event is InputEventMouseMotion:
+#		if event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
+#			position -= event.relative * zoom
